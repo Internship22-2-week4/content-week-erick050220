ce534b5552b6a3a137669501b1db260eb44355c7
erick050220 <elopezi2@miumg.edu.gt>
2022-07-12 01:18:35 -0600

    curso python

---
+++ day_1/Curso_Python/Basicos_del_lenguaje/clase7_variable_Expresiones.py
@@ -0,0 +1,21 @@
+#asignacion
+message = 'How are you?'
+_age = 20
+PI = 3.1416
+__do_not_touch = 'something important'
+
+#reasignacion
+my_var = 2
+my_var = my_var * 5
+print(my_var)
+print(__do_not_touch)
+
+# #Reglas de variables
+# -Pueden contener numeros y letras
+# -No pueden comenzar con numero
+# -Multiples palabras se unen con _
+# -No se pueden utilizar palabras reservadas
+# PEMDAS: truco para orden de las operaciones
+
+print(2+7/4)
+print((2+7)/4)
---
+++ day_1/Curso_Python/Basicos_del_lenguaje/clase9_Funciones.py
@@ -0,0 +1,7 @@
+def suma_de_dos_numeros(x,y):
+    return x + y
+
+
+print(suma_de_dos_numeros(10, 15))
+suma_total = suma_de_dos_numeros(10, 25)
+print(suma_total)
